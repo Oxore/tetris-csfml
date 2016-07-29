03e74d098cc9a0abda9d6d678089a0c6ce1b0066
oxore <oxore@protonmail.com>
2016-07-29 07:16:21 +0700

    Just added to github. Project is too raw.

---
+++ README.md
@@ -0,0 +1,4 @@
+This is unfinished tetris project written in pure C with CSFML-2.3-2 library.
+
+To compile source code I use following command:
+gcc -g main.c functions.c shape_maps.c -o debug/main -lcsfml-graphics -lcsfml-window -lcsfml-system
---
+++ functions.h
@@ -0,0 +1,19 @@
+
+/* Prototypes of functions*/
+void tKeyCtrl();
+void initAll();
+void tTick();
+void resetActiveShape();
+void putShape();
+int cellCollisionCheck(int dir);
+int wallCollisionCheck();
+int cellRotCollisionCheck();
+int wallRotCollisionCheck();
+void rotateShape();
+void rotateS1();
+void rotateS2();
+void rotateS3();
+void rotateS4();
+int linesRmScore();
+void scoreDisplay();
+void menuTick();
